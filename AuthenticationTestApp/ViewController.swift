ca7128676a86f8aa587f64a4e578752674538f96
Shuhei Yoshida <aneki525@gmail.com>
2021-02-01 11:46:20 +0900

    first commit

---
+++ AuthenticationTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuthenticationTestApp
+//
+//  Created by 吉田周平 on 2021/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
